59d119c202913a7721e22a635f5d8a89f2d93804
IbukunF <irf23@drexel.edu>
2019-01-22 07:34:42 -0500

    add script to move audio forward in time

---
+++ Drexel-VR-Tour/Assets/Forward.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Forward : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource sound;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Forward_5s);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Forward_5s()
+    {
+        sound.time += 5;
+    }
+}
