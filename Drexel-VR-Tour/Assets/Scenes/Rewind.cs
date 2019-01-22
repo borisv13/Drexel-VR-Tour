0b9440c3cfffdf637f13bd60c57781dc0c8285bf
IbukunF <irf23@drexel.edu>
2019-01-22 07:15:57 -0500

    add script to rewind audio on button press

---
+++ Drexel-VR-Tour/Assets/Scenes/Rewind.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rewind : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource sound;
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(rewind_5s);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void rewind_5s()
+    {
+        sound.time = sound.time - 5;
+    }
+}
