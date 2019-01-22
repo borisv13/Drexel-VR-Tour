e34c2ae3e8670ae42789abaf26cce75c0906fd0b
IbukunF <irf23@drexel.edu>
2019-01-22 05:10:21 -0500

    Add pause and play toggle button

---
+++ Drexel-VR-Tour/Assets/Pause_Audio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause_Audio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource sound;
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Pause_play);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Pause_play()
+    {
+        if (sound.isPlaying)
+        {
+            sound.Pause();
+        }
+        else
+        {
+            sound.Play();
+        }
+    }
+}
