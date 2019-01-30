762859ab2cf0dd43d1955599107fe8755328bfec
Kristófer Másson <kristoferma@me.com>
2019-01-29 19:02:52 -0500

    two scenes with hotspots

---
+++ Drexel-VR-Tour/Assets/Interactive360/Scripts/HotspotManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Interactive360
+{
+
+    //This script should be attached to an empty game object in any scene utilizing hotspots. It binds all hotspots in the scene to the scenes that they are supposed to load.
+    public class HotspotManager : MonoBehaviour
+    {
+
+        public Button[] m_hotSpotsInScene; //A reference to all the hotspots in the scene that would load new scenes
+        public GameObject m_playButton; //A reference to the button that toggles the video content to play
+        public GameObject m_pauseButton; //A reference to the button that toggle the video content to pause
+
+        //Toggle between showing play and pause button when once is pressed
+        public void toggleButton()
+        {
+
+            m_pauseButton.SetActive(!m_pauseButton.activeInHierarchy);
+            m_playButton.SetActive(!m_playButton.activeInHierarchy);
+
+        }
+
+    }
+}   
---
+++ Drexel-VR-Tour/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangeToKitchenScene()
+    {
+        SceneManager.LoadScene("Kitchen");
+    }
+
+    public void ChangeToDiningRoomScene()
+    {
+        SceneManager.LoadScene("DiningRoom");
+    }
+}
