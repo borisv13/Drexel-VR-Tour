a481e3de50dc85dad3b1e08ca2902e8a9df24ca7
TianJiaFeng <940880136@qq.com>
2019-02-24 22:55:25 -0500

    paly audio based on players' preference

--- Drexel-VR-Tour/Assets/Scripts/Audio.cs
+++ Drexel-VR-Tour/Assets/Scripts/Audio.cs
@@ -2,84 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Audio : MonoBehaviour
-{
-    //public int mainInteractiveItemIndex;
-
-    public AudioSource[] m_audio;
-    public GameObject[] m_play;
-    public GameObject[] m_pause;
-
-    private AudioSource currentSound = null;
-
-    //make sure main audio is the last audio
-    void Start()
+    public class Audio : MonoBehaviour
     {
-        int numberOfAudio = m_audio.Length;
-        currentSound = m_audio[numberOfAudio - 1];
-        m_play[numberOfAudio-1].SetActive(false);
-        m_pause[numberOfAudio-1].SetActive(true);
-    }
+        //public int mainInteractiveItemIndex;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!currentSound.isPlaying)
+        public AudioSource[] m_audio;
+        public GameObject[] m_play;
+        public GameObject[] m_pause;
+
+        private AudioSource currentSound = null;
+        private int autoPlayAudio = 1;
+
+        //make sure main audio is the last audio
+        void Start()
         {
-            if (currentSound.time == currentSound.clip.length)
+            autoPlayAudio = PlayerPrefs.GetInt("autoPlayAudio");
+            Debug.Log(autoPlayAudio);
+            int numberOfAudio = m_audio.Length;
+            currentSound = m_audio[numberOfAudio - 1];
+            if (autoPlayAudio == 1)
             {
-                int length = m_audio.Length;
-                for (int i = 0; i < length; i++)
+                m_play[numberOfAudio - 1].SetActive(false);
+                m_pause[numberOfAudio - 1].SetActive(true);
+                currentSound.Play();
+            }
+            else
+            {
+                m_play[numberOfAudio - 1].SetActive(true);
+                m_pause[numberOfAudio - 1].SetActive(false);
+            }
+            
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!currentSound.isPlaying)
+            {
+                if (currentSound.time == currentSound.clip.length)
                 {
-                    if (m_audio[i] == currentSound)
+                    int length = m_audio.Length;
+                    for (int i = 0; i < length; i++)
                     {
-                        m_audio[i].Pause();
-                        m_audio[i].time = 0;
-                        m_play[i].SetActive(true);
-                        m_pause[i].SetActive(false);
+                        if (m_audio[i] == currentSound)
+                        {
+                            m_audio[i].Pause();
+                            m_audio[i].time = 0;
+                            m_play[i].SetActive(true);
+                            m_pause[i].SetActive(false);
+                        }
                     }
                 }
             }
         }
-    }
 
 
-    public void openTextBox(int index)
-    {
-        int length = m_audio.Length;
-        for (int i = 0; i < length; i++)
-        {
-            if (index == i)
-            {
-                currentSound.Pause();
-                m_audio[i].Play();
-                currentSound = m_audio[i];
-                m_play[i].SetActive(false);
-                m_pause[i].SetActive(true);
-            }
-            else
-            {
-                m_play[i].SetActive(true);
-                m_pause[i].SetActive(false);
-            }
-        }
-    }
-
-    public void reloadInteractiveSound(int index)
-    {
-        m_audio[index].Pause();
-        m_audio[index].time = 0;
-    }
-
-    public void PlayAndPauseTextBox(int index)
-    {
-        if (m_audio[index].isPlaying)
-        {
-            m_audio[index].Pause();
-            m_play[index].SetActive(true);
-            m_pause[index].SetActive(false);
-        }
-        else
+        public void openTextBox(int index)
         {
             int length = m_audio.Length;
             for (int i = 0; i < length; i++)
@@ -99,31 +78,67 @@ public class Audio : MonoBehaviour
                 }
             }
         }
-    }
 
-    public void rewind(int index)
-    {
-        if (System.Math.Abs(m_audio[index].time) > 10)
+        public void reloadInteractiveSound(int index)
         {
-            m_audio[index].time = m_audio[index].time - 10;
-        }
-        else
-        {
+            m_audio[index].Pause();
             m_audio[index].time = 0;
         }
-    }
 
-    public void forward(int index)
-    {
-        if (m_audio[index].time < (m_audio[index].clip.length - 10))
+        public void PlayAndPauseTextBox(int index)
         {
-            m_audio[index].time += 10;
-            //m_audio[index].time = m_audio[index].clip.length - 10;
+            if (m_audio[index].isPlaying)
+            {
+                m_audio[index].Pause();
+                m_play[index].SetActive(true);
+                m_pause[index].SetActive(false);
+            }
+            else
+            {
+                int length = m_audio.Length;
+                for (int i = 0; i < length; i++)
+                {
+                    if (index == i)
+                    {
+                        currentSound.Pause();
+                        m_audio[i].Play();
+                        currentSound = m_audio[i];
+                        m_play[i].SetActive(false);
+                        m_pause[i].SetActive(true);
+                    }
+                    else
+                    {
+                        m_play[i].SetActive(true);
+                        m_pause[i].SetActive(false);
+                    }
+                }
+            }
         }
-        else
-        {
-            m_audio[index].time = m_audio[index].clip.length;
-        }
-    }
 
-}
+        public void rewind(int index)
+        {
+            if (System.Math.Abs(m_audio[index].time) > 10)
+            {
+                m_audio[index].time = m_audio[index].time - 10;
+            }
+            else
+            {
+                m_audio[index].time = 0;
+            }
+        }
+
+        public void forward(int index)
+        {
+            if (m_audio[index].time < (m_audio[index].clip.length - 10))
+            {
+                m_audio[index].time += 10;
+                //m_audio[index].time = m_audio[index].clip.length - 10;
+            }
+            else
+            {
+                m_audio[index].time = m_audio[index].clip.length;
+            }
+        }
+
+    }
+    
---
+++ Drexel-VR-Tour/Assets/Scripts/AutoAudio.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoAudio : MonoBehaviour
+{
+    public GameObject closeAutoAudio;
+    public GameObject openAutoAudio;
+    private int autoPlayAudio = 1;
+        // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("autoPlayAudio", autoPlayAudio);
+        closeAutoAudio.SetActive(true);
+        openAutoAudio.SetActive(false);
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void clickButton()
+    {
+        if (autoPlayAudio == 1)
+        {
+            autoPlayAudio = 0;
+            closeAutoAudio.SetActive(false);
+            openAutoAudio.SetActive(true);
+        }
+        else
+        {
+            autoPlayAudio = 1;
+            closeAutoAudio.SetActive(true);
+            openAutoAudio.SetActive(false);
+        }
+        PlayerPrefs.SetInt("autoPlayAudio", autoPlayAudio);
+    }
+
+    public int getAutoPlayAudio()
+    {
+        return autoPlayAudio;
+    }
+}
+
